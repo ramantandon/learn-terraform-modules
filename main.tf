79c4056c7def06019610cc6be7219fb48596b695
Raman Tandon <Raman.Tandon@mosaic.co>
2021-04-09 02:27:16 +0000

    changed AMI for us-east-1

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ module "ec2_instances" {
   name           = "my-ec2-cluster"
   instance_count = 1
 
-  ami                    = "ami-0c5204531f799e0c6"
+  ami                    = "ami-0742b4e673072066f"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [module.vpc.default_security_group_id]
   subnet_id              = module.vpc.public_subnets[0]
